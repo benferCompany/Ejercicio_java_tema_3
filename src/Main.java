5650991d152fd0cef99ca228126f29dd5003fa07
Benjamín Bernabé Gomez <benjidfer@gmail.com>
2023-03-16 17:14:26 -0300

    Ejercicio java tema 3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String[] nombre = {"h","o","l","a"};
+
+        String nombre_concatenado="";
+        for(int i =0;i<nombre.length;i++){
+            nombre_concatenado= nombre_concatenado+nombre[i];
+        }
+        System.out.println(nombre_concatenado);
     }
 }
